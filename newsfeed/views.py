ce8d896deb765dd218f6accb7d92467efa8da2f1
Temirlan504 <temirlan.ergazy@gmail.com>
2023-03-10 14:42:14 -0500

    Added Post model
    Added model (database table) for the posts and displaying them

--- newsfeed/views.py
+++ newsfeed/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 def main(request):
-    return render(request, "newsfeed/main.html")
+    return render(request, "newsfeed/main.html", {
+        "posts": Post.objects.all()
+    })
