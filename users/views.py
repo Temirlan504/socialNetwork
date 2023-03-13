1af88a22afaeeadb40e1cc12aa3a56eb5c461bd2
Temirlan504 <temirlan.ergazy@gmail.com>
2023-03-13 15:23:49 -0400

    Added feature to update profile
    Added feature to update profile

--- users/views.py
+++ users/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
+from .forms import UserUpdateForm, ProfileUpdateForm
 
 def profile_view(request):
     return render(request, "users/profile.html")
+
+def profile_update_view(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user) # "instance=" fills forms
+        p_form = ProfileUpdateForm(request.POST, 
+                                   request.FILES, 
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect("users:profile")
+    else:
+        u_form = UserUpdateForm(instance=request.user) # "instance=" fills forms
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    return render(request, "users/profile_update.html", {
+        "u_form": u_form,
+        "p_form": p_form
+    })
+
 
 def register_view(request):
     if request.method == "POST":
